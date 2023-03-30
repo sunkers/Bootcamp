bce0c968ee664082bb6fa6cd765a6eabc5492cd5
sunkers <mougin.loic@gmail.com>
2023-03-30 11:18:19 +0200

    encapsulation + main.js script

--- HTTP & API/getRequest.js
+++ HTTP & API/getRequest.js
@@ -1,15 +1,17 @@
 const XMLHttpRequest = require('xhr2');
 
 const xhr = new XMLHttpRequest();
-xhr.open('GET', 'https://www.google.com');
+const httprequest = function() {
+  xhr.open('GET', 'https://www.google.com');
+  xhr.onload = function() {
+    if (xhr.status === 200) {
+      console.log(xhr.responseText);
+    } else {
+      console.error('Error:', xhr.status);
+    }
+  };
+  xhr.send();
+}
 
-xhr.onload = function() {
-  if (xhr.status === 200) {
-    console.log(xhr.responseText);
-  } else {
-    console.error('Error:', xhr.status);
-  }
-};
-
-xhr.send();
+module.exports = httprequest;
 
---
+++ HTTP & API/main.js
@@ -0,0 +1,3 @@
+const getRequest = require('./getRequest.js');
+
+getRequest();
