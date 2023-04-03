c1f5fe80004a48d677fce513ff252d75e1f8422f
sunkers <mougin.loic@gmail.com>
2023-04-03 09:26:35 +0200

    Refoldering

---
+++ JAVA/Exercices/Singleton.java
@@ -0,0 +1,15 @@
+package com.iorga.app.singleton;
+
+public class Singleton {
+
+    private static Singleton instance;
+
+    private Singleton() { }
+
+    public static Singleton getInstance() {
+        // DEBUT Exercice 1
+        throw new RuntimeException("not implemented");
+        // FIN Exercice 1
+    }
+
+}
